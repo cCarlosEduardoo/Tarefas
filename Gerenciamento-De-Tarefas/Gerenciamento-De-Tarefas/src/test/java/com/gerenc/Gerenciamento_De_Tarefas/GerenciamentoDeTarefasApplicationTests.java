43de7c7c88d1ec0c81e8ca1f451fad5d32243aca
Carlos Eduardo <carloseduardotorresdocarmo@gmail.com>
2025-07-28 22:47:51 -0300

    first commit

---
+++ Gerenciamento-De-Tarefas/Gerenciamento-De-Tarefas/src/test/java/com/gerenc/Gerenciamento_De_Tarefas/GerenciamentoDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenc.Gerenciamento_De_Tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
